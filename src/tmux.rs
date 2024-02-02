146f3c125c07ddf2838db213eaba6b2980f03dfb
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-02-02 20:06:41 +0545

    basic tmux session switching done

--- src/tmux.rs
+++ src/tmux.rs
@@ -1,6 +1,9 @@
-use std::process::Command;
+use std::{
+    io::Result,
+    process::{self, Command},
+};
 
-fn get_tmux_sessions() -> (Vec<String>, String) {
+fn get_tmux_sessions() -> (Vec<String>, bool) {
     let output = Command::new("tmux")
         .arg("list-sessions")
         .output()
@@ -9,21 +12,31 @@ fn get_tmux_sessions() -> (Vec<String>, String) {
     let output = output.stdout;
     let output = String::from_utf8_lossy(&output);
 
+    let mut is_attached = false;
+
     let sessions = output
-        .split("\n")
-        .map(|s| s.to_string())
+        .lines()
+        .map(|s| {
+            let s_string = s.to_string();
+            let s_split = s_string.split(":").collect::<Vec<_>>();
+
+            if s.contains("(attached)") {
+                is_attached = true;
+            }
+
+            s_split[0].to_string()
+        })
         .collect::<Vec<String>>();
 
-    (sessions, output.to_string())
+    (sessions, is_attached)
 }
 
-pub fn goto_session(dirpath: &String) {
+pub fn goto_session(dirpath: &String) -> Result<()> {
     let replacable = vec!["."];
 
     let session_name = dirpath
         .chars()
         .map(|c| {
-            println!("'{c}'");
             if replacable.contains(&c.to_string().as_str()) {
                 "_".to_string()
             } else if c == '\\' {
@@ -35,9 +48,36 @@ pub fn goto_session(dirpath: &String) {
         .collect::<Vec<String>>()
         .join("");
 
-    // let (sessions, active) = get_tmux_sessions();
-    //
-    // println!("sessions: {:?}", sessions);
+    let (sessions, is_attached) = get_tmux_sessions();
 
-    println!("session_name: {}", session_name);
+    let is_already_open = sessions.contains(&session_name);
+
+    let output = if is_attached && is_already_open {
+        Command::new("sh")
+            .arg("-c")
+            .arg(format!("tmux switch-client -t {}", session_name))
+            .output()
+    } else if is_attached && !is_already_open {
+        Command::new("sh")
+            .arg("-c")
+            .arg(format!("tmux new -s {session_name} -c {session_name} -d && tmux switch-client -t {session_name}"))
+            .output()
+    } else if !is_attached && !is_already_open {
+        Command::new("sh")
+            .arg("-c")
+            .arg(format!("tmux new -s {session_name} -c {session_name}"))
+            .output()
+    } else if !is_attached && is_already_open {
+        Command::new("sh")
+            .arg("-c")
+            .arg(format!("tmux attach -t {}", session_name))
+            .output()
+    } else {
+        unreachable!()
+    }?;
+    if !output.status.success() {
+        process::exit(1);
+    }
+
+    Ok(())
 }
