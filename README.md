5d357c373d3feeb3fda1ab6a0d94f1dd0f9276e3
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-07-17 07:23:41 +0545

    start anew

--- README.md
+++
@@ -1,1 +0,0 @@
-# Smart Session Manager for tmux
--- src/utils.rs
+++
@@ -1,6 +0,0 @@
-pub fn remove_first_and_last(value: &str) -> String {
-    let mut chars = value.chars();
-    chars.next();
-    chars.next_back();
-    chars.as_str().to_string()
-}
